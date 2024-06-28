8ef3b4df7947ff7129af769361ebfb5b0729903f
Onixfff <60624718+Onixfff@users.noreply.github.com>
2024-06-28 11:55:09 +0200

    Вставил в доп проект

--- CheckPasses/Form1.Designer.cs
+++ CheckPasses/Form1.Designer.cs
@@ -476,7 +476,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Архив";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
--- CheckPasses/Program.cs
+++ CheckPasses/Program.cs
@@ -13,7 +13,7 @@ namespace CheckPasses
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1("conn5"));
+            Application.Run(new Form1());
         }
     }
 }
